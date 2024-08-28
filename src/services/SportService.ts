c5819bd60874adf6e9802aad124142179720a455
techatha <techatham_p@cmu.ac.th>
2024-08-28 19:55:44 +0700

    add 'SportService.ts' file and install axios

---
+++ src/services/SportService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/chartchai-class/PBandJ_data',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+})
+
+export default {
+    getMedalTally(limit: Number, page: Number) {
+        return apiClient.get('/countries?_limit=' + limit + '&_page=' + page)
+    },
+    getSportList() {
+        return apiClient.get('/sportLists')
+    }
+}
