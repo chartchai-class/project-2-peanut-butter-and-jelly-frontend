b81d8799a6015f883f3a72aafd52dd2a541ac468
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-09-06 17:59:46 +0700

    Revert "added pageSize"
    This reverts commit 8ae5f1a1a7e16ec5cb70147b8d79fc08778ae1b0.

--- src/views/MedalTallyView.vue
+++ src/views/MedalTallyView.vue
@@ -36,53 +36,6 @@ onMounted(() => {
   })
 })
 
-import { ref, computed, watch } from 'vue';
-
-const Countries = ref<Country[]>([]); // กำหนดให้ countries เป็นอาร์เรย์ของวัตถุประเภท Country
-
-const pageSize = ref<number | null>(null);
-const currentPage = ref(1);
-const error = ref<string | null>(null);
-
-watch(pageSize, (newSize) => {
-  if(newSize !== null && newSize > 9) {
-    pageSize.value = 9;
-  } else {
-    error.value = null;
-  }
-  currentPage.value = 1;
-});
-
-const totalPages = computed(() => Math.ceil(Countries.value.length / (pageSize.value || 1)));
-
-const paginatedCountries = computed(() => {
-  const size = pageSize.value || 9;
-  const startIndex = (currentPage.value - 1) * size;
-  const endIndex = startIndex + size;
-  return Countries.value.slice(startIndex, endIndex);
-});
-
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--;
-  }
-};
-
-const nextPage = () => {
-  if (currentPage.value < totalPages.value) {
-    currentPage.value++; 
-
-  }
-};
-
-// Watch for changes in pageSize and update pagination accordingly
-watch(pageSize, () => {
-  currentPage.value = 1; // Reset to the first page when pageSize changes
-});
-const countries = ref<Country[] | null>(null)
-SportService.getMedalTally(10,0).then((res) => {
-  countries.value = res.data
-});
 
 </script>
 <template>
@@ -107,23 +60,6 @@ SportService.getMedalTally(10,0).then((res) => {
         >Next Page &#62;</RouterLink
       >
     </div>
-  <div class="flex justify-between items-center mb-4">
-    <div class="pagination-controls">
-      <button @click="prevPage" :disabled="currentPage === 1">«</button>
-      <span>Page {{ currentPage }} of {{ totalPages }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages">»</button>
-    </div>
-    <div class="flex items-center">
-      <label for="pageSize" class="text-gray-700 mr-2">Number of countries per page: </label>
-      <input id="pageSize" type="number" min="1" max="9" v-model.number="pageSize" class="border border-gray-300 rounded p-2 mt-1 w-14"/>
-    <p v-if="error" class="text-red-500 ml-2"> {{ error  }}</p>
-    </div>
-  </div>
-
-    <div class="countries-list">
-      <CountryCard v-for="country in paginatedCountries" :key="country.id" :country="country" />
-    </div>
-
   <table class="min-w-full bg-white border border-gray-300">
     <thead>
       <tr class="bg-gray-200 text-gray-700 text-left">
