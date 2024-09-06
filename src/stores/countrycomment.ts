1f82475b1e8199e6d384333ab312bec6ac698b50
techatha <techatham_p@cmu.ac.th>
2024-09-06 12:59:17 +0700

    add comment section for each country

---
+++ src/stores/countrycomment.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Comment } from '@/types'
+
+let nextCommentId = 1 // Counter for generating unique IDs
+
+export const useCountryCommentStore = defineStore('countryCommentStore', () => {
+  const comments = ref<{ [sportId: string]: Comment[] }>({})
+
+  function addComment(sportId: string, comment: Omit<Comment, 'id'>) {
+    const id = nextCommentId++
+    const commentWithId: Comment = { id, ...comment }
+
+    if (!comments.value[sportId]) {
+      comments.value[sportId] = []
+    }
+    comments.value[sportId].push(commentWithId)
+  }
+
+  function fetchComments(sportId: string) {
+    return comments.value[sportId] || []
+  }
+
+  return { comments, addComment, fetchComments }
+})
