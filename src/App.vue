1ee59a77fe0afbe2c91ce0a68d5b890f944b5f9f
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-09-06 16:23:48 +0700

    fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <div class="flex h-screen">
     <nav class="w-1/4 bg-[rgb(231,224,224)] p-8 fixed h-full">
+      <div class="mb-6">
+        <h1 class="text-4xl font-bold mb-4 text-red-950">Olympic Report</h1>
+      </div>
       <ul>
         <li class="mb-4">
           <RouterLink
             :to="{ name: 'medal-tally-view' }"
-            class="block hover:bg-yellow-400 hover:rounded-lg hover:px-4 hover:py-2 transition-all"
+            class="block hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
             active-class="font-extrabold text-2xl rounded-lg px-4 py-2"
           >
             Medal Tally
@@ -18,19 +21,11 @@ import { RouterLink, RouterView } from 'vue-router'
         <li class="mb-4">
           <RouterLink
             :to="{ name: 'sport-list-table-view' }"
-            class="block hover:bg-yellow-400 hover:rounded-lg hover:px-4 hover:py-2 transition-all"
+            class="block hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
             active-class="font-extrabold text-2xl rounded-lg px-4 py-2"
           >
             List of Sports
           </RouterLink>
-        </li>
-        <li class="mb-4">
-          <a
-            href="#"
-            class="hover:bg-yellow-400 hover:rounded-lg hover:px-4 hover:py-2 transition-all"
-          >
-            About (Do we need this?? I don't want to work on it -Bay)
-          </a>
         </li>
       </ul>
     </nav>
