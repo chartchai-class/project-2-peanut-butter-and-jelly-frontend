567b0139197bf3b04515e7045cb33d676c9a1a86
pubr-wthan <120130146+pubr-wthan@users.noreply.github.com>
2024-09-07 15:00:38 +0700

    add routerlink in app.vue

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,16 @@ import { RouterLink, RouterView } from 'vue-router'
       <ul>
         <li class="mb-4">
           <RouterLink
+            :to="{ name: 'about-view' }"
+            class="block hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
+            active-class="font-extrabold text-2xl rounded-lg px-4 py-2"
+          >
+            About 
+          </RouterLink>
+        </li>
+
+        <li class="mb-4">
+          <RouterLink
             :to="{ name: 'medal-tally-view' }"
             class="block hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
             active-class="font-extrabold text-2xl rounded-lg px-4 py-2"
--- src/views/Aboutview.vue
+++ src/views/Aboutview.vue
@@ -1,2 +1,9 @@
 <script setup lang="ts">
+
+
+
+
+
+
+
 </script>
