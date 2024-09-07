0cf32dcd6a5dec30296d782f63f3df3fdba9511d
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-09-07 15:19:40 +0700

    added description on AboutView.vue

--- src/views/Aboutview.vue
+++ src/views/Aboutview.vue
@@ -1,3 +1,6 @@
 <template>
-    <h1> hello</h1>
-</template>
+    <div class="about p-6 max-w-4xl mx-auto text-center">
+      <h1 class="text-3xl font-bold mb-4">A site that displays Olympic Report Information. </h1>
+      <p class="text-lg text-gray-700 mb-6"> This holds data on countries that participated on the Olympics and their athletic achievements.</p>
+    </div>
+  </template>
