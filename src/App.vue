e233fbe6a55f98f6c92dc405c84b2596bf034326
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-10-26 20:33:45 +0700

    add svg

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ import { RouterLink, RouterView } from 'vue-router'
             class="block hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
             active-class="font-extrabold text-2xl rounded-lg px-4 py-2"
           >
-            Medal Tally
+          Medal Tally
           </RouterLink>
         </li>
         <li class="mb-4">
@@ -40,9 +40,12 @@ import { RouterLink, RouterView } from 'vue-router'
         <li class="mb-4">
         <RouterLink
             :to="{ name: 'login-view' }"
-            class="block text-center bg-gray-200 text-gray-600 font-extrabold text-2xl rounded-lg px-4 py-2  hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
+            class="flex items-center justify center text-center bg-gray-200 text-gray-600 font-extrabold text-2xl rounded-lg px-4 py-2  hover:bg-red-950 hover:text-white hover:rounded-lg hover:px-4 hover:py-2 transition-all"
             active-class="bg-red-950 text-white font-extrabold text-2xl rounded-lg px-4 py-2"
           >
+          <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16zm0-10a2 2 0 11-4 0 2 2 0 014 0zm0 6c-1.66 0-3-1.34-3-3h6c0 1.66-1.34 3-3 3z" />
+          </svg>
             Login
           </RouterLink>
         </li>
